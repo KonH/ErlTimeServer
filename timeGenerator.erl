d9c87fc248318381503f49084e2e5fd7ea848977
KonH <konh@yandex.ru>
2016-12-25 21:44:14 +0700

    timeServer -> timeGenerator; timeServer for server startup mock;

---
+++ timeGenerator.erl
@@ -0,0 +1,16 @@
+-module(timeGenerator).
+-export([getTime/0]).
+
+getTime() ->
+	getTextTime().
+
+getTextTime() -> 
+	{Date, Time} = getRawTime(),
+	{Year, Month, Day} = Date,
+	{Hour, Minute, Second} = Time,
+	io:format(
+	  '~4..0b-~2..0b-~2..0bT~2..0b:~2..0b:~2..0b+00:00', 
+	  [Year, Month, Day, Hour, Minute, Second]).
+
+getRawTime() ->
+	calendar:universal_time().
