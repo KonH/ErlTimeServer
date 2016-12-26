292cb85b1522d455702242623066882ba6ab70c4
KonH <konh@yandex.ru>
2016-12-26 17:41:25 +0700

    Stop function;

--- timeServer.erl
+++ timeServer.erl
@@ -1,5 +1,5 @@
 -module(timeServer).
--export([start/0, ask/1]).
+-export([start/0, ask/1, stop/1]).
 
 start() ->
 	Pid = spawn(fun loop/0),
@@ -10,6 +10,9 @@ loop() ->
 		{From, time} ->
 			From ! {self(), timeGenerator:getTime()},
 			loop();
+		{From, stop} ->
+			From ! {self(), exit},
+			exit(normal);
 		{From, Other} ->
 			From ! {self(), {error, Other}},
 			loop()
@@ -17,6 +20,9 @@ loop() ->
 
 ask(Pid) ->
 	rpc(Pid, time, 1000).
+
+stop(Pid) ->
+	rpc(Pid, stop, 1000).
 
 rpc(Pid, Request, Timeout) ->
 	Pid ! {self(), Request},
