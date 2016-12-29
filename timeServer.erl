48db9400993a6ce6217588571563f5ac2f9ec20c
KonH <konh@yandex.ru>
2016-12-29 23:06:19 +0700

    Track worker Pid, restart after it fails;

--- timeServer.erl
+++ timeServer.erl
@@ -1,9 +1,33 @@
 -module(timeServer).
--export([start/0, ask/1, stop/1]).
+-export([start/0, ask/0, stop/0]).
 
 start() ->
 	Pid = spawn(fun loop/0),
+	onExit(Pid, fun onWorkerExit/2),
+	register(timeServerWorker, Pid),
 	Pid.
+
+onExit(Pid, Fun) ->
+	spawn(fun() ->
+		process_flag(trap_exit, true),
+		link(Pid),
+		receive
+		{'EXIT', Pid, Why} ->
+			Fun(Pid, Why)
+		end
+	end).
+
+onWorkerExit(Pid, Why) ->
+	io:format("Worker ~p died with:~p~n",[Pid, Why]),
+	case Why of 
+		normal ->
+			io:format('Server is stopped.~n');
+		_ ->
+			io:format('Restarting server.~n'),
+			NewPid = start(),
+			io:format('New pid: ~p~n', [NewPid])
+	end.
+
 
 loop() -> 
 	receive
@@ -15,14 +39,25 @@ loop() ->
 			exit(normal);
 		{From, Other} ->
 			From ! {self(), {error, Other}},
-			loop()
+			exit(error);
+		_ ->
+			exit(error)
 	end.
 
-ask(Pid) ->
-	rpc(Pid, time, 1000).
+ask() ->
+	rpc(time).
 
-stop(Pid) ->
-	rpc(Pid, stop, 1000).
+stop() ->
+	rpc(stop).
+
+rpc(Request) ->
+	Pid = whereis(timeServerWorker),
+	case Pid of
+		undefined ->
+			io:format('Server isn\'t started.~n');
+		_ ->
+			rpc(Pid, Request, 1000)
+	end.
 
 rpc(Pid, Request, Timeout) ->
 	Pid ! {self(), Request},
