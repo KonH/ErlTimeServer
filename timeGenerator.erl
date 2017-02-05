e06659d3ed15ac3e9e67d774691a544725353a1b
KonH <konh@yandex.ru>
2017-02-05 14:53:32 +0700

    timeWebServer to web server mode;

--- timeGenerator.erl
+++ timeGenerator.erl
@@ -8,9 +8,10 @@ getTextTime() ->
 	{Date, Time} = getRawTime(),
 	{Year, Month, Day} = Date,
 	{Hour, Minute, Second} = Time,
-	io:format(
-	  '~4..0b-~2..0b-~2..0bT~2..0b:~2..0b:~2..0b+00:00', 
-	  [Year, Month, Day, Hour, Minute, Second]).
+	io_lib:format(
+		'~4..0b-~2..0b-~2..0bT~2..0b:~2..0b:~2..0b+00:00', 
+		[Year, Month, Day, Hour, Minute, Second]
+	).
 
 getRawTime() ->
 	calendar:universal_time().
---
+++ timeWebServer.erl
@@ -0,0 +1,25 @@
+-module(timeWebServer).
+-export([start/1, stop/0, time/3, test1/2]).
+
+start(Port) ->
+	inets:start(),
+	{ok, Pid} = 
+		inets:start(
+			httpd,
+			[
+				{port, Port},
+				{server_name, "timeWebServer"}, 
+				{server_root,"."},
+				{document_root,"."},
+				{modules,[mod_esi]},
+				{erl_script_alias, {"/time", [timeWebServer]}},
+				{bind_address, "localhost"}
+			]),
+	Pid.
+
+stop() ->
+	inets:stop().
+
+time(SessID, _Env, _Input) ->
+	Time = timeGenerator:getTime(),
+	ok = mod_esi:deliver(SessID, [Time]).
